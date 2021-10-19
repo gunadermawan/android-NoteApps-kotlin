52fbd454c852565b79df47a6dfe32c414c058aaa
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-19 13:00:19 +0700

    membuat instance databse dan controller database

--- app/src/main/java/com/gunder/mynoteapps/db/NoteHelper.kt
+++ app/src/main/java/com/gunder/mynoteapps/db/NoteHelper.kt
@@ -3,12 +3,32 @@ package com.gunder.mynoteapps.db
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import com.gunder.mynoteapps.db.DatabaseContract.NoteColumns.Companion.TABLE_NAME
+import java.sql.SQLException
 
 
 class NoteHelper(context: Context) {
+    //    constructor class
     private var databaseHelper: DatabaseHelper = DatabaseHelper(context)
     private lateinit var database: SQLiteDatabase
-    companion object{
+
+    companion object {
         private const val DATABASE_TABLE = TABLE_NAME
+    }
+
+    //    instance database
+    private var INSTANCE: NoteHelper? = null
+    fun getInstance(context: Context): NoteHelper =
+        INSTANCE ?: synchronized(this) {
+            INSTANCE ?: NoteHelper(context)
+        }
+
+    //    untuk controller databse(open/close database)
+    @Throws(SQLException::class)
+    fun open() {
+        database = databaseHelper.writableDatabase
+    }
+
+    fun close() {
+        database.close()
     }
 }
