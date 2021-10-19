eb662e31277cedc1518b7ae4ff86a5dfcbca55a0
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-19 12:56:32 +0700

    menambhkan constructor

--- app/src/main/java/com/gunder/mynoteapps/db/NoteHelper.kt
+++ app/src/main/java/com/gunder/mynoteapps/db/NoteHelper.kt
@@ -1,7 +1,14 @@
 package com.gunder.mynoteapps.db
 
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import com.gunder.mynoteapps.db.DatabaseContract.NoteColumns.Companion.TABLE_NAME
+
 
 class NoteHelper(context: Context) {
-
+    private var databaseHelper: DatabaseHelper = DatabaseHelper(context)
+    private lateinit var database: SQLiteDatabase
+    companion object{
+        private const val DATABASE_TABLE = TABLE_NAME
+    }
 }
